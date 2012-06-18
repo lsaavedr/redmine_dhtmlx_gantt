49d009e802d40546d035f9ed446f33a0b041a91e
Luis Saavedra <luis94855510@gmail.com>
2012-06-17 22:11:21 -0400

    Adding stylesheets

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -4,7 +4,6 @@ class PollsController < ApplicationController
   before_filter :find_project, :authorize, :only => [:index, :vote]
 
   def index
-    @project = Project.find(params[:project_id])
     @polls = Poll.all
   end
 
