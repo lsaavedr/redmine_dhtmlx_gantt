8587967f4d2148d0a5fc49eb6b9c2e4548c47fd1
Luis Saavedra <luis94855510@gmail.com>
2012-06-20 23:20:11 -0400

    dhtmlxCalendar example!

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-match 'projects/:project_id/polls/:id/:action', :to => 'polls'
+get 'projects/:project_id/polls/:action', :to => 'polls'
+post 'projects/:project_id/polls/:id/:answer/vote', :to => 'polls#vote'
